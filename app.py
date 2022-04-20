7fd702b5d0770f002f33a065fed00fc662991af1
Neba Roland <53044894+Roland-coder@users.noreply.github.com>
2022-04-20 22:22:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -34,70 +34,12 @@ text_pred = st.text_input("Please enter news article in the text below")
 df = pd.read_csv("https://jeanbucket001.s3.us-west-2.amazonaws.com/train.csv.zip" )
 
 
-def cleaning_text(text):
-    stop_words = stopwords.words("english")
-
-    # removing urls from tweets
-    text = re.sub(r'http\S+', " ", text)    
-    # remove mentions
-    text = re.sub(r'@\w+',' ',text)         
-    # removing hastags
-    text = re.sub(r'#\w+', ' ', str(text))       
-    # removing html tags
-    text = re.sub('r<.*?>',' ', text)       
-    
-    # removing stopwords stopwords 
-    text = text.split()
-    text = " ".join([word for word in text if not word in stop_words])
-
-    for punctuation in string.punctuation:
-        text = text.replace(punctuation, "")
-    
-    return text
-
-df['text'] = df['text'].replace(np.nan, '').apply(lambda x: cleaning_text(x)) 
-new_text = cleaning_text(text_pred)
-
-  
-def tokenizer(x_train, y_train, newv, max_len_word):
-    # because the data distribution is imbalanced, "stratify" is used
-    X_train, X_val, y_train, y_val = train_test_split(x_train, y_train, 
-                                                      test_size=.2, shuffle=True, 
-                                                      stratify=y_train, random_state=0)
-
-    # Tokenizer
-    tokenizer = Tokenizer(num_words=5000)
-    tokenizer.fit_on_texts(X_train)
-    sequence_dict = tokenizer.word_index
-    word_dict = dict((num, val) for (val, num) in sequence_dict.items())
-
-    # Sequence data
-    train_sequences = tokenizer.texts_to_sequences(X_train)
-    train_padded = pad_sequences(train_sequences,
-                                 maxlen=max_len_word,
-                                 truncating='post',
-                                 padding='post')
-    X_val[len(X_val)] = newv
-    val_sequences = tokenizer.texts_to_sequences(X_val)
-    val_padded = pad_sequences(val_sequences,
-                                maxlen=max_len_word,
-                                truncating='post',
-                                padding='post', )
-   
-
-    print(train_padded.shape)
-    print(val_padded.shape)
-    print('Total words: {}'.format(len(word_dict)))
-    return train_padded, val_padded, y_train, y_val, word_dict
-
-X_train, X_val, y_train, y_val, word_dict = tokenizer(df.text, df.label, new_text, 300)
-
 
 if st.button('Predict News article  text'):
 	if len(new_text) <= 2:
 		st.write("Please enter a valid news article")
 	else:
-		pred = model.predict(X_val[-1])
+		pred = model.predict([text_pred])
 		pred = np.argmax(pred, axis = 0)
 		if pred == 0:
 			st.write("News Article is Reliable")
